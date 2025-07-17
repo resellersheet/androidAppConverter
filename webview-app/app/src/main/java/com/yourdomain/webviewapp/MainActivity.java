af16cf9389b3c5695ae2d6b37ec9c1621e96780f
resellersheet <resellersheet@gmail.com>
2025-07-16 17:00:57 -0700

    Update MainActivity.java

--- webview-app/app/src/main/java/com/yourdomain/webviewapp/MainActivity.java
+++ webview-app/app/src/main/java/com/yourdomain/webviewapp/MainActivity.java
@@ -1,35 +1,26 @@
+package com.yourdomain.webviewapp;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private WebView webView;
-
+public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // your layout containing a WebView
+        setContentView(R.layout.activity_splash);
 
-        webView = findViewById(R.id.webview);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        ImageView logo = findViewById(R.id.logoImageView);
+        Glide.with(this)
+             .load(R.drawable.ic_launcher)  // your launcher icon here or downloaded logo
+             .into(logo);
 
-        // Load URL from strings.xml
-        String url = getString(R.string.site_url);
-
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl(url);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if(webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+        // Delay and then start MainActivity (simplified)
+        logo.postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, 1500);
     }
 }
