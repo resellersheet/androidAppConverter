4be5be92afc11bb87f60de6e93798c2ed11d631f
resellersheet <resellersheet@gmail.com>
2025-07-20 16:37:52 -0700

    Update MainActivity.java

--- webview-app/app/src/main/java/com/yourdomain/webviewapp/MainActivity.java
+++ webview-app/app/src/main/java/com/yourdomain/webviewapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.yourdomain.webviewapp;
 
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Build;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -14,6 +18,26 @@ public class MainActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.webview);
         String url = getString(R.string.site_url);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            // For older devices
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            // For Android 7.0+ devices
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        });
+
         webView.loadUrl(url);
     }
 }
