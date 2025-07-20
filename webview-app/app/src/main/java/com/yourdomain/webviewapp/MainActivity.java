dd48f678f96ebe361ec1484d7b389a70751b0af3
resellersheet <resellersheet@gmail.com>
2025-07-20 16:48:43 -0700

    Update MainActivity.java

--- webview-app/app/src/main/java/com/yourdomain/webviewapp/MainActivity.java
+++ webview-app/app/src/main/java/com/yourdomain/webviewapp/MainActivity.java
@@ -1,15 +1,20 @@
 package com.yourdomain.webviewapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,24 +22,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webview);
-        String url = getString(R.string.site_url);
+        progressBar = findViewById(R.id.progressBar);
 
+        String url = getString(R.string.site_url);
         webView.getSettings().setJavaScriptEnabled(true);
 
         webView.setWebViewClient(new WebViewClient() {
-            // For older devices
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
             }
 
-            // For Android 7.0+ devices
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 view.loadUrl(request.getUrl().toString());
                 return true;
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress < 100) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(newProgress);
+                } else {
+                    progressBar.setProgress(100);
+                    progressBar.setVisibility(View.GONE);
+                }
             }
         });
 
